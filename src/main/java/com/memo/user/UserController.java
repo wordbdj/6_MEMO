d45ac566777ee0a8ef7bec25592b06ef07f507a4
DESKTOP-C5GBA64\user <test@test.com>
2024-07-12 18:50:21 +0900

    2024-07-12

--- src/main/java/com/memo/user/UserController.java
+++ src/main/java/com/memo/user/UserController.java
@@ -8,12 +8,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
 	
+	/**
+	 * 회원가입 화면
+	 * @return
+	 */
 	@GetMapping("/sign-up-view")
 	public String signUpView() {
 		// 가운데 레이아웃 조각만 내려주면 전체 레이아웃으로 구성된다.
 		return "user/signUp";
 	}
 	
+	/**
+	 * 로그인 화면
+	 * @return
+	 */
 	@GetMapping("/sign-in-view")
 	public String signInView() {
 		return "user/signIn";
