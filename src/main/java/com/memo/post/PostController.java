757c13598e83b6208619b54aa5e5864df58b358a
DESKTOP-C5GBA64\user <test@test.com>
2024-07-26 17:45:39 +0900

    2024-07-26

--- src/main/java/com/memo/post/PostController.java
+++ src/main/java/com/memo/post/PostController.java
@@ -22,7 +22,10 @@ public class PostController {
 	private PostBO postBO;
 
 	@GetMapping("/post-list-view")
-	public String postListView(HttpSession session, Model model) {
+	public String postListView(
+			@RequestParam(value = "prevId", required = false) Integer prevIdParam,
+			@RequestParam(value = "nextId", required = false) Integer nextIdParam,
+			HttpSession session, Model model) {
 		// 로그인 여부 확인
 		Integer userId = (Integer) session.getAttribute("userId");
 		if (userId == null) {
@@ -31,10 +34,30 @@ public class PostController {
 		}
 
 		// DB 조회 - 글 목록
-		List<Post> postList = postBO.getPostListByUserId(userId);
-
+		List<Post> postList = postBO.getPostListByUserId(userId, prevIdParam, nextIdParam);
+		int prevId = 0;
+		int nextId = 0;
+		if (postList.isEmpty() == false) { // 글목록이 비어있지 않을 떄 페이징 정보 세팅
+			prevId = postList.get(0).getId(); // 첫번째칸 id 
+			nextId = postList.get(postList.size()- 1).getId(); // 마지막 칸 id
+			
+			// 이전 방향의 끝인가? 그러면 0
+			// prevId와 테이블의 제일 큰 숫자와 같으면 이전의 끝 페이지
+			if(postBO.isPrevLastPageByUserId(userId, prevId)) {
+				prevId = 0;
+			}
+			
+			// 다음 방향의 끝인가? 그러면 0
+			// nextId와 테이블의 제일 작은 숫자와 같으면 다음의 끝 페이지
+			if (postBO.isNextLastPageByUSerId(userId, nextId)) {
+				nextId = 0;
+			}
+			
+		}
 		// 모델에 담기
 		model.addAttribute("postList", postList);
+		model.addAttribute("prevId", prevId);
+		model.addAttribute("nextId", nextId);
 
 		return "post/postList";
 	}
--- src/main/java/com/memo/post/bo/PostBO.java
+++ src/main/java/com/memo/post/bo/PostBO.java
@@ -1,6 +1,7 @@
 package com.memo.post.bo;
 
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +27,57 @@ public class PostBO {
 	@Autowired
 	private FileManagerService fileManagerService;
 	
+	// 페이징 정보 필드 (limit)
+	private static final int POST_MAX_SIZE = 3;
+	
 	// input: 로그인 된 사람의 userId
 	// output: List<Post>
-	public List<Post> getPostListByUserId(int userId){
-		return postMapper.selectPostListByUserId(userId);
+	public List<Post> getPostListByUserId(int userId, Integer prevId, Integer nextId){
+		
+		// 게시글 번호 10 9 8 | 7 6 5 | 4 3 2 | 1
+		// 만약 내가 4 3 2 페이지에 있을 때
+		// 1) 다음: 2보다 작은 3개 DESC
+		// 2) 이전: 4보다 큰 3개 ASC => 5 6 7 => BO에서 reverse 7 6 5
+		// 3) 페이징 X: 최신순 3 DESC
+		// 가장 좋은 방법은 따로따로 만드는 것이다
+		Integer standardId = null; // 기준 postId
+		String direction = null; // 방향
+		if (prevId != null) { // 2) 이전
+			standardId = prevId;
+			direction = "prev";
+			
+			List<Post> postList = postMapper.selectPostListByUserId(userId, standardId, direction, POST_MAX_SIZE);
+			Collections.reverse(postList);
+			
+			return postList;
+		} else if (nextId != null) { // 1) 다음 
+			standardId = nextId;
+			direction = "next";
+		}
+		
+		// 3) 페이징 X, 1) 다음
+		return postMapper.selectPostListByUserId(userId, standardId, direction, POST_MAX_SIZE);
 	}
+	
+	// 이전 페이지의 마지막인가?
+	
+	public boolean isPrevLastPageByUserId(int userId,int prevId) {
+		
+		int maxPostId = postMapper.selectPostIdByUserIdAsSort(userId, "DESC");
+		return maxPostId == prevId; // 같으면 마지막
+	}
+	
+	public boolean isNextLastPageByUSerId(int userId,int nextId) {
+		
+		int minPostId = postMapper.selectPostIdByUserIdAsSort(userId, "ASC");
+		return minPostId == nextId; // 같으면 마지막
+	}
+	
+	
+	// 다음 페이지의 마지막인가?
+	
+	
+	
 	
 	public void addPost(int userId, String userLoginId, 
 			String subject, String content, MultipartFile file) {
