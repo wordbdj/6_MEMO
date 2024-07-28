ba160ca2373b4a6fef892fb4b86828e17cdf23dd
DESKTOP-OQIQ52P\FORYOUCOM <jun062202@gmail.com>
2024-07-28 12:33:03 +0900

    2021-07-28

--- src/main/java/com/memo/config/WebMvcConfig.java
+++ src/main/java/com/memo/config/WebMvcConfig.java
@@ -1,15 +1,31 @@
 package com.memo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.memo.common.FileManagerService;
+import com.memo.interceptor.PermissionInterceptor;
 
 @Configuration // 설정을 위한 Spring bean
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	// 이미지 path와 서버에 업로드 된 실제 이미지와 매핑 설정
+	
+	@Autowired
+	private PermissionInterceptor interceptor;
+	
+	// 인터셉터 설정
+	@Override 
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+		.addInterceptor(interceptor)
+		.addPathPatterns("/**")
+		.excludePathPatterns("/error", "/css/**","/img/**", "/user/sign-out"); // /img -> static 밑에 있는 img
+		
+	}
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
---
+++ src/main/java/com/memo/interceptor/PermissionInterceptor.java
@@ -0,0 +1,64 @@
+package com.memo.interceptor;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component // spring bean
+public class PermissionInterceptor implements HandlerInterceptor{
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws IOException {
+		
+		// 요청 url path를 꺼낸다
+		String uri = request.getRequestURI();
+		log.info("[@@@@ preHandle] uri:{}", uri);
+		
+		// 로그인 여부를 꺼낸다.
+		HttpSession session = request.getSession();
+		Integer userId = (Integer)session.getAttribute("userId");
+		
+		// 비로그인 && /post -> 로그인 페이지로 이동, 컨트롤러 수행 방지
+		if(userId == null && uri.startsWith("/post")) {
+			response.sendRedirect("/user/sign-in-view");
+			return false; // 원래 요청 주소에 대한 컨트롤러 수행 X
+		}
+		
+		// 로그인 && /user -> 글목록 페이지로 이동, 컨트롤러 수행 방지
+		if(userId != null && uri.startsWith("/user")) {
+			response.sendRedirect("/post/post-list-view");
+			return false;
+		}
+		
+		return true; // 컨트롤러 수행 true
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView mav) {
+		
+		// view, model 객체가 있다는 건 html이 해석되기 전
+		log.info("[$$$$$ postHandle]");
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			Exception ex) {
+		
+		// html이 완성된 상태
+		log.info("[##### afterCompletion]");
+		
+	}
+
+}
